6942b0c3f8446a8ef94f4d7f33164be0de8a2b0a
Kamar1k <artem.mironov61@mail.ru>
2025-09-18 20:12:56 +0700

    добавлены классы Item и Customer

---
+++ Model/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ObjectOrientedPractics.Model
+{
+    internal class Customer
+    {
+        private readonly int _id;
+        private string _fullname;
+        private string _address;
+
+        public string FullName
+        {
+            get { return _fullname; }
+            set { _fullname = value; }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public Customer(string fullname, string address)
+        {
+            _fullname = fullname;
+            _address = address;
+        }
+    }
+}
--- Model/Item.cs
+++ Model/Item.cs
@@ -13,5 +13,30 @@ namespace ObjectOrientedPractics.Model
         private string _info;
         private double _cost;
 
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Info
+        {
+            get { return _info; }
+            set { _info = value; }
+        }
+        public double Cost  
+        {
+            get { return _cost; } 
+            set {_cost = value; } 
+        }
+
+        public Item(string name, string info, double cost)
+        {
+            _name = name;
+            _info = info;
+            _cost = cost;
+        }
     }
 }
+
