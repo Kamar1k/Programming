b6ceb72c58a48211cfee2a604cb3b00e26f601a5
Kamar1k <artem.mironov61@mail.ru>
2025-09-19 03:49:38 +0700

    попытка Сверстать ItemsTab и какие-то беды с MainForms и Designer

---
+++ View/Tabs/ItemsTab.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace ObjectOrientedPractics.View.Tabs
+{
+    partial class ItemsTab
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            label1 = new Label();
+            label2 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel4 = new Panel();
+            panel3 = new Panel();
+            button1 = new Button();
+            panel6 = new Panel();
+            panel5 = new Panel();
+            button2 = new Button();
+            button3 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel4.SuspendLayout();
+            panel6.SuspendLayout();
+            panel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Items.AddRange(new object[] { "ItemsListBox" });
+            listBox1.Location = new Point(3, 27);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(320, 334);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(3, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Items";
+            label1.Click += label1_Click_1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(3, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Selected Item";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Window;
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(335, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(446, 409);
+            panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(panel5);
+            panel2.Controls.Add(panel6);
+            panel2.Controls.Add(listBox1);
+            panel2.Controls.Add(panel3);
+            panel2.Controls.Add(panel4);
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(326, 409);
+            panel2.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(button1);
+            panel4.Location = new Point(3, 362);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(103, 44);
+            panel4.TabIndex = 3;
+            panel4.Paint += panel4_Paint;
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(128, 280);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(91, 44);
+            panel3.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(97, 38);
+            button1.TabIndex = 0;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(button2);
+            panel6.Location = new Point(112, 362);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(103, 44);
+            panel6.TabIndex = 6;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(button3);
+            panel5.Location = new Point(220, 362);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(103, 44);
+            panel5.TabIndex = 7;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(3, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(97, 38);
+            button2.TabIndex = 1;
+            button2.Text = "Remove";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(3, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(97, 38);
+            button3.TabIndex = 1;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 40);
+            label3.Name = "label3";
+            label3.Size = new Size(21, 15);
+            label3.TabIndex = 3;
+            label3.Text = "ID:";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 71);
+            label4.Name = "label4";
+            label4.Size = new Size(34, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Cost:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 104);
+            label5.Name = "label5";
+            label5.Size = new Size(42, 15);
+            label5.TabIndex = 5;
+            label5.Text = "Name:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(3, 216);
+            label6.Name = "label6";
+            label6.Size = new Size(70, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Description:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(3, 234);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(440, 90);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(45, 37);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(129, 23);
+            textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(45, 68);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(129, 23);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(3, 122);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(440, 82);
+            textBox4.TabIndex = 10;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // ItemsTab
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "ItemsTab";
+            Size = new Size(784, 415);
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Label label1;
+        private Label label2;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel4;
+        private Panel panel3;
+        private Button button1;
+        private Panel panel5;
+        private Button button3;
+        private Panel panel6;
+        private Button button2;
+        private Label label4;
+        private Label label3;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label6;
+        private Label label5;
+    }
+}
---
+++ View/Tabs/ItemsTab.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ObjectOrientedPractics.View.Tabs
+{
+    public partial class ItemsTab : UserControl
+    {
+        public ItemsTab()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
